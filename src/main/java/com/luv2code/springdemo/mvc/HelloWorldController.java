ecd32082fb4d1a47f90b41d771d98d9a6cb3d8cc
Sandy Woods <sandywoods@Sandys-Air-2.attlocal.net>
2022-06-09 18:57:16 -0400

    Should show @RequestMapping && @RequestParm

--- src/main/java/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ src/main/java/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -5,8 +5,10 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloWorldController {
 
 	// need a controller method to show the initial HTML form
@@ -22,7 +24,7 @@ public class HelloWorldController {
 	public String processForm() {
 		return "helloworld";
 	}
-
+	
 	// new a controller method to read form data and
 	// add data to the model
 
@@ -43,6 +45,25 @@ public class HelloWorldController {
 				
 		return "helloworld";
 	}
+	
+	@RequestMapping("/processFormVersionThree")	
+	public String processFormVersionThree(
+			@RequestParam("studentName") String theName, 
+			Model model) {
+				
+		// convert the data to all caps
+		theName = theName.toUpperCase();
+		
+		// create the message
+		String result = "Hey My Friend from v3! " + theName;
+		
+		// add message to the model
+		model.addAttribute("message", result);
+				
+		return "helloworld";
+	}	
+	
+	
 	
 }
 
--- src/main/webapp/WEB-INF/view/main-menu.jsp
+++ src/main/webapp/WEB-INF/view/main-menu.jsp
@@ -1,4 +1,4 @@
-<!DOCTYPE>
+<!DOCTYPE html>
 <html>
 
 <body>
@@ -7,7 +7,16 @@
 
 <hr>
 
-<a href="showForm">Hello World form</a>
+<a href="hello/showForm">Hello World form</a>
+
+
 </body>
 
 </html>
+
+
+
+
+
+
+
